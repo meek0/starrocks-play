3560cae3a4580ae49334612ac01d5f9afe5e48a9
meek0 <cedricgfont@gmail.com>
2024-04-25 13:42:31 -0400

    fix missing file close

--- starrocks_play/script.py
+++ starrocks_play/script.py
@@ -51,6 +51,7 @@ def get_a_query_from(directory, ids_file):
     if (os.path.isdir(directory) and os.path.isfile(ids_file)):
         i = open(ids_file, "r")
         ids = i.readlines()
+        i.close()
 
         random_file = random.choice(os.listdir(directory))
 
